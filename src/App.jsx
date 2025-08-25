ce97975987a6a8c623365490971f0fd5de035a3c
MohamedSaabith342 <e21342@eng.pdn.ac.lk>
2025-08-25 20:15:41 +0530

    created basics of ecoomerce-website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { Routes, Route } from 'react-router'
+import './App.css'
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route 
+        path='/' 
+        element={<HomePage/> }>
+      </Route>
+      <Route 
+        path='checkout' 
+        element={<CheckoutPage />}>
+      </Route>
+      <Route 
+        path='orders' 
+        element={<OrdersPage />}>
+      </Route>
+
+
+    </Routes>
+   
+  )
+}
+
+export default App
