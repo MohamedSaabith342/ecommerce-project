60b118664511fecd23517ebf77f069e9c6cafc99
MohamedSaabith342 <e21342@eng.pdn.ac.lk>
2025-08-29 23:52:54 +0530

    Generate HTML For Payment Summary

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -5,12 +5,19 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
+
 export function CheckoutPage({ cart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null);
   useEffect(() => {
     axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
       .then((response) => {
         setDeliveryOptions(response.data);
+      })
+
+    axios.get('/api/payment-summary')
+      .then((response) => {
+        setPaymentSummary(response.data);
       })
   }, [])
   return (
@@ -42,7 +49,7 @@ export function CheckoutPage({ cart }) {
         <div className="checkout-grid">
           <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-              const selectDeliveryOption = deliveryOptions.find((deliveryOption)=>{
+              const selectDeliveryOption = deliveryOptions.find((deliveryOption) => {
                 return deliveryOption.id === cartItem.deliveryOptionId
               })
               return (
@@ -80,14 +87,14 @@ export function CheckoutPage({ cart }) {
                         Choose a delivery option:
                       </div>
                       {deliveryOptions.map((deliveryOption) => {
-                          let priceString = "FREE Shipping";
-                          if(deliveryOption.priceCents > 0) {
-                            priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
-                          }
+                        let priceString = "FREE Shipping";
+                        if (deliveryOption.priceCents > 0) {
+                          priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
+                        }
 
                         return (
                           <div key={deliveryOption.id} className="delivery-option">
-                            <input type="radio" checked={deliveryOption.id ==cartItem.deliveryOptionId}
+                            <input type="radio" checked={deliveryOption.id == cartItem.deliveryOptionId}
                               className="delivery-option-input"
                               name={`delivery-option-${cartItem.productId}`} />
                             <div>
@@ -101,7 +108,7 @@ export function CheckoutPage({ cart }) {
                           </div>
                         )
                       })}
-                      
+
                     </div>
                   </div>
                 </div>
@@ -115,34 +122,39 @@ export function CheckoutPage({ cart }) {
               Payment Summary
             </div>
 
-            <div className="payment-summary-row">
-              <div>Items (3):</div>
-              <div className="payment-summary-money">$42.75</div>
-            </div>
+            {paymentSummary && (
+              <>
+                <div className="payment-summary-row">
+                  <div>Items ({paymentSummary.totalItems})</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.productCostCents)}</div>
+                </div>
 
-            <div className="payment-summary-row">
-              <div>Shipping &amp; handling:</div>
-              <div className="payment-summary-money">$4.99</div>
-            </div>
+                <div className="payment-summary-row">
+                  <div>Shipping &amp; handling:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.shippingCostCents)}</div>
+                </div>
 
-            <div className="payment-summary-row subtotal-row">
-              <div>Total before tax:</div>
-              <div className="payment-summary-money">$47.74</div>
-            </div>
+                <div className="payment-summary-row subtotal-row">
+                  <div>Total before tax:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostBeforeTaxCents)}</div>
+                </div>
 
-            <div className="payment-summary-row">
-              <div>Estimated tax (10%):</div>
-              <div className="payment-summary-money">$4.77</div>
-            </div>
+                <div className="payment-summary-row">
+                  <div>Estimated tax (10%):</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.taxCents)}</div>
+                </div>
 
-            <div className="payment-summary-row total-row">
-              <div>Order total:</div>
-              <div className="payment-summary-money">$52.51</div>
-            </div>
+                <div className="payment-summary-row total-row">
+                  <div>Order total:</div>
+                  <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
+                </div>
 
-            <button className="place-order-button button-primary">
-              Place your order
-            </button>
+                <button className="place-order-button button-primary">
+                  Place your order
+                </button>
+              </>
+            )}
+
           </div>
         </div>
       </div>
